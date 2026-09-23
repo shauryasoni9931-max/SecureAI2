cb9e73dda8ab9f019ad16c04008d97190fcd1210
shauryasoni9931-max <shauryasoni9931@gmail.com>
2026-09-23 21:13:10 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('secureai-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './app.js',
+      './manifest.json'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
